26845c659109ec2f9ab45069656d6ead949ff746
Robert OBrien <robert.obrien@westpoint.edu>
2024-11-04 09:31:17 -0500

    Tim you still suck

--- todo/todo_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("tasks/", views.all_tasks, name="all_tasks"),
-    path("tasks/<int:task_id>/", views.task, name="task"),
-]
